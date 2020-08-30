ac785cf3d25aa4de88d23bb710dda027c2d62469
Ivana Livaja <ivana.livaja@hotmail.com>
2020-08-30 20:50:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import * as points from './data/features.geojson';
+import * as myData from './data/features.json';
+import Sidebar from './components/Sidebar';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const App = () => {
+	const [ data ] = useState(myData.features);
+	const mapContainerRef = useRef(null);
+
+	// initialize map when component mounts
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: mapContainerRef.current, // container id
+			style: 'mapbox://styles/bincica/ckaf60zrv03j61il9fee0hmn4', // stylesheet location
+			center: [ 18.690563, 45.559239 ], // starting position [lng, lat]
+			zoom: 15,
+			minZoom: 14,
+			maxZoom: 18,
+			hash: true
+		});
+
+		map.on('load', () => {
+			// add the data source for new a feature collection with no features
+			map.addSource('points', {
+				type: 'geojson',
+				data: points
+			});
+			// now add the layer, and reference the data source above by name
+			map.addLayer({
+				id: 'points',
+				source: 'points',
+				type: 'symbol',
+				layout: {
+					'icon-image': 'town-hall-11',
+					'icon-padding': 0,
+					'icon-allow-overlap': true
+				}
+			});
+		});
+
+		// add navigation control (the +/- zoom buttons)
+		map.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
+
+		// clean up on unmount
+		return () => map.remove();
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+	return (
+		<div className="App">
+			<div className="map-container" ref={mapContainerRef} />
+			<Sidebar data={data} />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import '../styles/Sidebar.css';
+
+function Sidebar(props) {
+	return (
+		<Fragment>
+			<div id="top-menu" className="z2 p0 clearfix mb0 fixed left-0 right-0 top-0 bg-white border-bottom">
+				<ul className="left list-reset m0">
+					<li className="inline-block md-show btn btn-project m0">Popis</li>
+					<li className="inline-block md-show btn btn-project m0">Arhitekti</li>
+					<li className="inline-block md-show btn btn-project m0">Stil</li>
+					<li className="inline-block md-show btn btn-project m0">Namjena</li>
+				</ul>
+			</div>
+
+			<div id="sidebar" className="relative" mode="list">
+				<div id="sidebar-list" className="bg-white absolute top-0 bottom-0 left-0 right-0 border-left">
+					<div id="list-view" className="absolute top-0 bottom-0 left-0 right-0 overflow-auto">
+						<ul id="heritage-list" className="m0 list-reset border-top">
+							{props.data.map((point, index) => {
+								console.log(point.properties);
+								return (
+									<li id="ninkn1" className="card border-bottom relative" key={index}>
+										<p className="card_title my0 mx3"> {point.properties['građevina']} </p>
+										<p className="my0 mx3 darkgray"> {point.properties.adresa} </p>
+										<p className="my0 mx3 darkgray"> {point.properties.Mjesto} </p>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
+}
+
+export default Sidebar;
